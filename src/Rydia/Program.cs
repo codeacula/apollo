88384a73817f365a4e3fbd2b48bb4fbe94987cfb
Codeacula <codeacula@codeacula.com>
2025-09-16 17:06:45 -0500

    I have components showing up now

--- src/Rydia/Program.cs
+++ src/Rydia/Program.cs
@@ -1,53 +1,74 @@
+using NetCord;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Rest;
+using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Hosting.Services.ComponentInteractions;
+using NetCord.Services.ComponentInteractions;
 using Quartz;
 using Rydia;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services
-    .AddControllers();
-
-builder.Services
-    .AddHttpApplicationCommands();
-
-builder.Services
-.AddDiscordGateway(options =>
+try
 {
-    options.Intents = GatewayIntents.All;
-})
-.AddGatewayHandlers(typeof(IRydiaApp).Assembly);
+    var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("Rydia") ?? throw new NullReferenceException();
+    builder.Services
+        .AddControllers();
 
-builder.Services
-    .AddQuartz(q =>
+    builder.Services
+    .AddDiscordGateway(options =>
     {
-        q.UsePersistentStore(s =>
-        {
-            s.UseProperties = true;
-            s.UsePostgres(connectionString);
-            s.UseSystemTextJsonSerializer();
-        });
+        options.Intents = GatewayIntents.All;
     })
-    .AddQuartzHostedService(opt =>
+        .AddApplicationCommands()
+        .AddDiscordRest()
+        .AddComponentInteractions<ButtonInteraction, ButtonInteractionContext>()
+        .AddComponentInteractions<StringMenuInteraction, StringMenuInteractionContext>()
+        .AddComponentInteractions<UserMenuInteraction, UserMenuInteractionContext>()
+        .AddComponentInteractions<RoleMenuInteraction, RoleMenuInteractionContext>()
+        .AddComponentInteractions<MentionableMenuInteraction, MentionableMenuInteractionContext>()
+        .AddComponentInteractions<ChannelMenuInteraction, ChannelMenuInteractionContext>()
+        .AddComponentInteractions<ModalInteraction, ModalInteractionContext>()
+        .AddGatewayHandlers(typeof(IRydiaApp).Assembly);
+
+    var connectionString = builder.Configuration.GetConnectionString("Rydia") ?? throw new NullReferenceException();
+
+    builder.Services
+        .AddQuartz(q =>
+        {
+            q.UsePersistentStore(s =>
+            {
+                s.UseProperties = true;
+                s.UsePostgres(connectionString);
+                s.UseSystemTextJsonSerializer();
+            });
+        })
+        .AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
+
+    var app = builder.Build();
+
+    app.AddModules(typeof(IRydiaApp).Assembly);
+    app.UseRequestLocalization();
+
+    if (app.Environment.IsDevelopment())
     {
-        opt.WaitForJobsToComplete = true;
-    });
+        app.MapOpenApi();
+    }
 
-var app = builder.Build();
+    app.MapControllers();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+    app.UseHttpsRedirection();
+
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    await app.RunAsync();
 }
-
-app.MapControllers();
-
-app.UseHttpsRedirection();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-await app.RunAsync();
+catch (Exception ex)
+{
+    await Console.Error.WriteLineAsync(ex.ToString());
+}
