8a27ee6431fcf7897ac40461b490cd784c81106f
Codeacula <codeacula@codeacula.com>
2025-12-13 04:30:31 -0600

    Remove redundant test for job failure in CreateToDoCommandHandlerTests

--- tests/Apollo.Application.Tests/ToDos/CreateToDoCommandHandlerTests.cs
+++ tests/Apollo.Application.Tests/ToDos/CreateToDoCommandHandlerTests.cs
@@ -63,46 +63,4 @@ public class CreateToDoCommandHandlerTests
     scheduler.Verify(x => x.GetOrCreateJobAsync(reminderDate, It.IsAny<CancellationToken>()), Times.Exactly(2));
     store.Verify(x => x.SetReminderAsync(It.IsAny<ToDoId>(), reminderDate, quartzJobId, It.IsAny<CancellationToken>()), Times.Once);
   }
-
-  [Fact]
-  public async Task HandleWhenEnsureJobFailsReturnsError()
-  {
-    var store = new Mock<IToDoStore>();
-    var scheduler = new Mock<IToDoReminderScheduler>();
-    var handler = new CreateToDoCommandHandler(store.Object, scheduler.Object);
-
-    var personId = new PersonId(Guid.NewGuid());
-    var description = new Description("test");
-    var reminderDate = DateTime.UtcNow.AddMinutes(5);
-    var quartzJobId = new QuartzJobId(Guid.NewGuid());
-
-    _ = store
-      .Setup(x => x.CreateAsync(It.IsAny<ToDoId>(), personId, description, It.IsAny<CancellationToken>()))
-      .ReturnsAsync((ToDoId id, PersonId pId, Description desc, CancellationToken _) => Result.Ok(new ToDo
-      {
-        CreatedOn = new CreatedOn(DateTime.UtcNow),
-        Description = desc,
-        Energy = new Energy(0),
-        Id = id,
-        Interest = new Interest(0),
-        PersonId = pId,
-        Priority = new Priority(0),
-        UpdatedOn = new UpdatedOn(DateTime.UtcNow)
-      }));
-
-    _ = scheduler
-      .SetupSequence(x => x.GetOrCreateJobAsync(reminderDate, It.IsAny<CancellationToken>()))
-      .ReturnsAsync(Result.Ok(quartzJobId))
-      .ReturnsAsync(Result.Fail<QuartzJobId>("boom"));
-
-    _ = store
-      .Setup(x => x.SetReminderAsync(It.IsAny<ToDoId>(), reminderDate, quartzJobId, It.IsAny<CancellationToken>()))
-      .ReturnsAsync(Result.Ok());
-
-    var result = await handler.Handle(new CreateToDoCommand(personId, description, reminderDate), CancellationToken.None);
-
-    Assert.True(result.IsSuccess);
-    Assert.NotEmpty(result.Errors);
-    scheduler.Verify(x => x.GetOrCreateJobAsync(reminderDate, It.IsAny<CancellationToken>()), Times.Exactly(2));
-  }
 }
